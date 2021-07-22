789892ae424fda6b8a147df9891b08e384e8d21d
myuu <myusa.95.222@gmail.com>
2021-07-22 20:40:17 +0900

    足し算と引き算を追加

--- main.go
+++ main.go
@@ -3,8 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
+	"unicode"
 )
+
+func strtoi(s string) (int, string) {
+	var res = 0
+	for i, c := range s {
+		if !unicode.IsDigit(c) {
+			return res, s[i:]
+		}
+		res = res*10 + int(c) - int('0')
+	}
+	return res, ""
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -14,11 +25,27 @@ func main() {
 	fmt.Println(".intel_syntax noprefix")
 	fmt.Println(".globl main")
 	fmt.Println("main:")
-	n, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "引数の形式が数値ではありません")
+
+	n, input := strtoi(os.Args[1])
+	fmt.Printf("  mov rax, %d\n", n)
+
+	for input != "" {
+		var c = input[0]
+		if c == '+' {
+			input = input[1:]
+			n, input = strtoi(input)
+			fmt.Printf("  add rax, %d\n", n)
+			continue
+		}
+		if c == '-' {
+			input = input[1:]
+			n, input = strtoi(input)
+			fmt.Printf("  sub rax, %d\n", n)
+			continue
+		}
+		fmt.Fprintf(os.Stderr, "予期しない文字です: '%c'\n", c)
 		os.Exit(1)
 	}
-	fmt.Printf("  mov rax, %d\n", n)
+
 	fmt.Println("  ret")
 }
